4ff6a42237535d6590c88d73ae238809c3c5c7e8
Alexis <batista29alexis@gmail.com>
2026-04-01 20:22:39 -0500

    feat(ui): use alerts for displaying game results

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function getComputerChoice() {
 
 // Get a choice from the player
 function getPlayerChoice() {
-    let selection = Number(prompt("Choose!\n\n1. Rock\n2. Paper\n3. Scissors"))
+    let selection = Number(prompt("Choose!\n\n1. Rock 🪨\n2. Paper 📄\n3. Scissors ✂️"))
     switch(selection) {
         case 1: return "Rock";
         case 2: return "Paper";
@@ -27,13 +27,16 @@ function playRound(playerChoice, computerChoice) {
         playerChoice === "Scissors" && computerChoice === "Paper"
     ) {
         playerScore++;
+        alert(`You win!\n\nYour Choice: ${playerChoice}\nComputer choice: ${computerChoice}`)
         console.log(`You win! ${playerChoice} beats ${computerChoice}`)
     }
     else if (playerChoice === computerChoice) {
+        alert(`It's a tie!\n\nYour Choice: ${playerChoice}\nComputer choice: ${computerChoice}`)
         console.log("It's a tie!")
     }
     else {
         computerScore++;
+        alert(`You lost!\n\nYour Choice: ${playerChoice}\nComputer choice: ${computerChoice}`)
         console.log(`You lost! ${computerChoice} beats ${playerChoice}`)
     }
 }
@@ -42,28 +45,30 @@ function playRound(playerChoice, computerChoice) {
 function playGame() {
     for (let i = 0; i < 5; i++) {
         if (playerScore === 3 || computerScore === 3) {
-            console.log("")
+            break;
         }
 
         const playerSelection = getPlayerChoice();  
         const computerSelection = getComputerChoice();
 
         console.clear(); // Clear console before show the next round result
-
+        console.log("Your choice: " + playerSelection);
         console.log("Computer choice: " + computerSelection);
-        console.log("Player choice: " + playerSelection);
 
         playRound(playerSelection, computerSelection)
     }
     // Decide who have won this BO5 game of Rock Paper Scissors
     console.clear();
     if (playerScore > computerScore) {
+        alert(`You have won this BO5 game of Rock Paper Scissors!\n\nYour score: ${playerScore}\nComputer score: ${computerScore}`)
         console.log("You have won this BO5 game of Rock Paper Scissors!")
     }
     else if (playerScore === computerScore) {
+        alert(`This BO5 game of Rock Paper Scissors has ended in a tie!\n\nYour score: ${playerScore}\nComputer score: ${computerScore}`)
         console.log("This BO5 game of Rock Paper Scissors has ended in a tie!")
     }
     else {
+        alert(`We can not win every time... you lost!\n\nYour score: ${playerScore}\nComputer score: ${computerScore}`)
         console.log("We can not win every time... you lost!")
     }
 }
